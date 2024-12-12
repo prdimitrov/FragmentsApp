8735d7453849f06f152602014bfa5e9911f4aa77
prdimitrov <prdimitrov97@gmail.com>
2024-12-12 20:23:01 +0200

    Initial commit and add Fragment

---
+++ app/src/main/java/com/example/fragmentsapp/FirstFragment.java
@@ -0,0 +1,13 @@
+package com.example.fragmentsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+}
