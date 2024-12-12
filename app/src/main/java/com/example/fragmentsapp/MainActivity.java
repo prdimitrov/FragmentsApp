2899228de3c28f74fa7be6e1b8721ec01d13beac
prdimitrov <prdimitrov97@gmail.com>
2024-12-12 21:01:07 +0200

    Add Fragments in MainActivity - initialize buttons, on click listeners for the buttons and a loadFragment method in MainActivity.java class.

--- app/src/main/java/com/example/fragmentsapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentsapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.fragmentsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,34 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            btn1 = findViewById(R.id.btn1);
+            btn2 = findViewById(R.id.btn2);
+
+            btn1.setOnClickListener(v1 -> {
+                loadFragment(new FirstFragment());
+            });
+
+            btn2.setOnClickListener(v2 -> {
+                loadFragment(new SecondFragment());
+            });
+
             return insets;
         });
+    }
+
+    public void loadFragment(Fragment fragment) {
+        /*
+        Responsible for all runtime management of fragments
+        including adding, removing, hiding, showing
+        and navigating between fragments!
+         */
+        FragmentManager fm = getSupportFragmentManager();
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        //Replace the framelayout with new fragment
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 }
